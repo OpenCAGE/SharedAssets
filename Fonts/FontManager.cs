0040d2deb9e2aee58b36d3664f73fc3d40d54786
MattFiler <matthew.filer@hotmail.co.uk>
2020-11-22 16:01:58 +0000

    Added initial shared assets

---
+++ Fonts/FontManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+
+static class FontManager
+{
+    static PrivateFontCollection fonts;
+
+    static FontManager()
+    {
+        fonts = new PrivateFontCollection();
+    }
+
+    static public void AddFont(byte[] fontData)
+    {
+        IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
+        Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+        fonts.AddMemoryFont(fontPtr, fontData.Length);
+        Marshal.FreeCoTaskMem(fontPtr);
+    }
+
+    static public Font GetFont(int index, int size)
+    {
+        return new Font(fonts.Families[index], size);
+    }
+}
